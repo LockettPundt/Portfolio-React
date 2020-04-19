fbdb9f34defbfb5aba8ed4c3999de8939c56995f
Lockett Pundt <kalifornium@hotmail.com>
2020-04-19 13:51:54 -0400

    imported the sidebar and content components. these will likely be the only components in the App.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Content from './components/Content';
+import SideBar from './components/SideBar';
+import { AppBox } from './Style/StyledComponents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBox className="App">
+      <SideBar />
+      <Content />
+    </AppBox>
   );
 }
 
