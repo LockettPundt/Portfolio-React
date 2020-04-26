cea1df17af1e949f5cba07dc245ad25de922ea83
Lockett Pundt <kalifornium@hotmail.com>
2020-04-26 09:44:56 -0400

    imported AboutMe component.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ContentBox, InnerContentBox } from '../Style/ContentContainers';
 import Greeting from './Greeting';
+import Picture from './Picture';
+import AboutMe from './AboutMe';
 
 const Content = () => (
   <ContentBox>
     <InnerContentBox>
       <Greeting />
+      <Picture />
+      <AboutMe />
       <p>{'this is the content box'.repeat(700)}</p>
 
     </InnerContentBox>
