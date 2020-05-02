ce3ecd4dd51fffd28f94f4d40c979c40308978cf
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:35:12 -0400

    projects component. I'll create a single project component that will be used for each one within this component.

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import SectionTitle from './SectionTitle';
+import SectionContainer from './SectionContainer';
+
+
+const Projects = () => (
+  <SectionContainer>
+    <SectionTitle title="Projects" />
+  </SectionContainer>
+);
+
+
+export default Projects;
