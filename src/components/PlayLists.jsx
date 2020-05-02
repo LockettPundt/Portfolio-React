bd81b24cfb76364a3bcce4a254aa2d555fbda1b4
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:37:11 -0400

    playlists component. i'll make individual components for each one.

---
+++ src/components/PlayLists.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import SectionContainer from './SectionContainer';
+import SectionTitle from './SectionTitle';
+
+const Playlist = ({ children }) => (
+  <SectionContainer>
+    <SectionTitle title="Playlists" />
+  </SectionContainer>
+
+);
+
+export default Playlist;
