006c3bcc0cc93a77e9adfc6b4d4d5491f490c081
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:39:19 -0400

    working on media queries and adjusting for different screen sizes.

--- src/Style/SideBarStyling.js
+++ src/Style/SideBarStyling.js
@@ -1,23 +1,35 @@
 import styled from 'styled-components';
 
 export const SideBarDiv = styled.div`
-  background-color: var(--green);
+  background-color: ${(props) => (props.darkMode ? 'rgb(38, 38, 39)' : 'rgb(125, 187, 145)')};
   display: flex;
   flex-direction: column;
   justify-content: center;
-  /* min-width: 360px; */
-  width: 25%;
-  line-height: 0.8rem;
+  min-width: 351px;
+  line-height: 0.5rem;
   font-size: 1.45rem;
+  color: ${(props) => (props.darkMode ? '#ffc89f' : 'rgb(38, 38, 39)')};
   height: 100%;
   margin: auto 0rem;
   overflow: hidden;
+  transition: 0.4s ease-in-out;
+  
+  
+  
+  @media screen and (max-width: 991px) {
+    font-size: 0.9rem;
+    min-width: 260px;
+  }
+  
+  @media screen and (max-width: 800px) {
+    min-width: 240px;
+  }
+  
   @media screen and (max-width: 600px) {
       position: fixed;
       width: 100%;
       height: auto;
-      font-size: 2.7rem;
-      padding: 2rem;
+      font-size: 1rem;
     }
 `;
 
@@ -26,29 +38,50 @@ export const NameBox = styled.div`
   display: flex;
   flex-direction: column;
   background-color: var(green);
-  width: auto;
   padding: 0rem 1.5rem;
-
   .indent {
     padding-left: 2.8rem;
+  }
+  
+  @media screen and (max-width: 600px) {
+    padding: 0.5rem 1rem 0.1rem 1rem;
+    line-height: 0.1rem;
   }
 
 `;
 
 export const SocialIcons = styled.div`
-  font-size: 2.3rem;
-  padding-left: 1.5rem;
-
+  font-size: 2.6rem;
+  padding-left: 1.7rem;
+  margin: 1rem 0;
+  
+  @media screen and (max-width: 600px) {
+    padding: 0rem 1rem;
+  }
+  
+  > i {
+    @media screen and (max-width: 975px) {
+      font-size: 1.6rem;
+    }
+    @media screen and (max-width: 600px) {
+      font-size: 1.6rem;
+    }
+  }
 
   a {
-    font-family: 'Lato' , sans-serif;
     text-decoration: none;
-    color: var(--black);
-    margin-right: 0.5rem;
+    transition: 0.4s ease-in-out;
+    color: ${(props) => (props.darkMode ? '#ffc89f' : 'rgb(38, 38, 39)')};
+    margin-right: 0.8rem;
     
-    @media screen and (max-width: 600px) {
-      margin-right: 2rem;
+    @media screen and (max-width: 975px) {
+      font-size: 1.6rem;
     }
+    @media screen and (max-width: 600px) {
+      margin-right: 1rem;
+      font-size: 1.6rem;
+    }
+  }
 
 }
 
@@ -58,8 +91,4 @@ export const SocialIcons = styled.div`
   transition: 0.3s ease-in-out;
   }
 
-  @media screen and (max-width: 600px) {
-      font-size: 5.7rem;
-      
-    }
 `;
