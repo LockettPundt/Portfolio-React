04ac90c75e2ee5343a8c0a2eaaa5ef7b836f2a18
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:40:35 -0400

    context added for dark mode. media queries added for different screen sizes.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,46 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import SectionTitle from './SectionTitle';
+import SectionContainer from './SectionContainer';
 
-
-const AboutDiv = styled.div`
-  font-size: inherit;
-  margin: var(--sectionMargin);
-  
-  .title {
-    font-size: 2rem;
-    font-weight: 300;
-    width: 20%;
-    
-    @media screen and (max-width: 600px) {
-      font-size: 3rem;
-      width: 30%;
-    }
-  }
-  
-  div {
-    display: flex;
-    justify-content: center;
-    align-self: center;
-  }
-  hr {
-    width: 80%;
-    align-self: center;
-    margin: auto;
-    
-    
-    @media screen and (max-width: 600px) {
-      width: 70%;
-    }
-  }
-`;
 
 const AboutMe = () => (
-  <AboutDiv>
-    <div>
-      <p className="title">About Me</p>
-      <hr noshade />
-    </div>
-
+  <SectionContainer>
+    <SectionTitle title="About Me" />
     <p>
       I'm an enthusiastic Web/Software Developer who has turned a curiosity
       into a reality. I enrolled at DigitalCrafts in January of 2020 to
@@ -55,7 +20,7 @@ const AboutMe = () => (
       such that it was a seemless transition to jump right in. I'm very excited to begin
       a new chapter and continue learning.
     </p>
-  </AboutDiv>
+  </SectionContainer>
 );
 
 
