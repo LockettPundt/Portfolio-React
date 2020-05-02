fc50f91dff6147c26794e606b2661ab60e76422e
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:38:14 -0400

    imported useReducer, created an initialState that checks localstorage for a value for darkmode. wrapped app in stateprovider.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useReducer } from 'react';
+import { StateProvider } from './context';
 import Content from './components/Content';
 import SideBar from './components/SideBar';
 import AppBox from './Style/MainContainers';
+import Reducer from './reducers/Reducer';
+
+const status = localStorage.getItem('darkMode') || false;
+const initialState = {
+  darkMode: status === 'true',
+};
+console.log(localStorage.getItem('darkMode'));
+console.log('app initialstate status', initialState.darkMode);
 
 function App() {
   return (
-    <AppBox>
-      <SideBar />
-      <Content />
-    </AppBox>
+    <StateProvider value={useReducer(Reducer, initialState)}>
+      <AppBox>
+        <SideBar />
+        <Content />
+      </AppBox>
+    </StateProvider>
+
   );
 }
 
