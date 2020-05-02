306a07f0f72af7ca7165d6eae9490777df84fb2b
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 17:38:54 -0400

    added some media queries and passed the components props to change on darkmode.

--- src/Style/ContentContainers.js
+++ src/Style/ContentContainers.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
-
-export const ContentBox = styled.div`
-  background-color: var(--grey);
-  width: 75%;
+const grey = 'rgb(241, 236, 228)';
+const black = 'rgb(70, 70, 70)';
+export const ContentBox = styled.section`
+  background-color: ${(props) => (props.darkMode ? black : grey)};
+  transition: 0.4s ease-in-out;
   height: auto;
+  color: ${(props) => (props.darkMode ? '#ffc89f' : 'rgb(38, 38, 39)')};
   overflow-y: scroll;
+  scroll-behavior: smooth;
   
   @media screen and (max-width: 600px) {
     width: 100%;
@@ -19,42 +22,18 @@ export const InnerContentBox = styled.div`
   height: 100vh;
   margin: 0 auto 3rem auto;
   overflow: visible;
-  font-size: 1.3rem;
-  
-  @media screen and (max-width: 600px) {
-    font-size: 2rem;
-  }
+  font-size: 1.6rem;
   
   @media screen and (max-width: 400px) {
-    font-size: 2.3rem;
+    font-size: 1.2rem;
   }
-  
+
 `;
 
 export const GreetingBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 31vh auto 40rem auto;
-  
-  @media screen and (max-width: 1780px) {
-    margin: 33vh auto 52vh auto; 
-  }
-  
-  @media screen and (max-width: 1450px) {
-    margin: 36vh auto 52vh auto;
-  }
-  
-  @media screen and (max-width: 1150px) {
-    margin: 40vh auto 52vh auto;
-  }
-  
-  @media screen and (max-width: 800px) {
-    margin: 43vh auto 50vh auto;
-  }
-  
-  @media screen and (max-width: 600px) {
-    margin: 50vh auto 50vh auto;  
-  }
+  margin: 35vh auto 40rem auto;
 
 `;
