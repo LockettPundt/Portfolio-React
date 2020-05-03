d550014c8002ee76c42fdae808362c4cd22963d9
Lockett Pundt <kalifornium@hotmail.com>
2020-05-02 22:09:20 -0400

    brought the overall font size down on the greeting on mobile.

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,35 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { GreetingBox } from '../Style/ContentContainers';
-
 import StateContext from '../context';
 
+
+const GreetingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 35vh auto 40rem auto;
+  
+  @media screen and (max-width: 769px) {
+    margin: 39vh auto 40rem auto;
+  }
+  
+`;
+
 const DownArrow = styled.i`
+  transition: 0.4s ease-in-out;
   color: ${(props) => (props.darkMode ? '#ffc89f' : 'rgb(38, 38, 39)')};
   font-size: 3rem;
+  
   &:hover {
   color: var(--hoverPink);
   transform: scale(1.3);
   transition: 0.3s ease-in-out;
   }
+  @media screen and (max-width: 600px) {
+    font-size: 2.5rem;
+  }
+  
 `;
 
 export const Welcome = styled.h1`
@@ -22,14 +40,14 @@ export const Welcome = styled.h1`
     font-size: 2.4rem;
     
   }
-  @media screen and (max-width: 767px) {
+  @media screen and (max-width: 769px) {
     font-size: 2rem;
     
   }
   @media screen and (max-width: 600px) {
-    font-size: 3rem;
-    
+    font-size: 1.9rem;
   }
+  
 `;
 
 const Greeting = () => {
