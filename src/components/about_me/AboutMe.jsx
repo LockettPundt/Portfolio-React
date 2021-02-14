0c1da67a55ad827fe73cde84938de3428f1b0f77
Lockett Pundt <kalifornium@hotmail.com>
2021-02-14 13:13:21 -0500

    refactor(about me): :speech_balloon: text changes

--- src/components/about_me/AboutMe.jsx
+++ src/components/about_me/AboutMe.jsx
@@ -7,22 +7,14 @@ const AboutMe = () => (
   <SectionContainer>
     <SectionTitle title="About Me" />
     <p>
-      I&apos;m an enthusiastic Web/Software Developer who has turned a curiosity
-      into a reality. I enrolled in DigitalCrafts&apos;s immersive Full Stack Web Development
-      course in January of 2020 to realize a growing interest.
-    </p>
-    <p>
       I&apos;m a musician at heart who has been extremely fortunate to travel the world
       playing music for most of my adult life. I&apos;ve had an incredible journey and
       made invaluable friends along the way. I have also fallen for programming and
       creating things digitally. The parallels between music and Web Development are
       such that it was a seemless transition to jump right in. The creative process, the
       &apos;aha!&apos; moments and the satisfaction of honing a skill through hard work and
-      study, all echo back to my journey with music and songwriting. The pursuit of growing as
-      a developer is very exciting and it&apos;s wild to look back just a few months to see the
-      parallax of what I knew then, versus what I know now (with also being more aware of how much I
-      don&apos;t know!). I&apos;m very much looking forward to learning more and pushing
-      myself ahead.
+      study, all echo back to my journey with music and songwriting. I look forward to 
+      where this new chapter takes me...
     </p>
   </SectionContainer>
 );
